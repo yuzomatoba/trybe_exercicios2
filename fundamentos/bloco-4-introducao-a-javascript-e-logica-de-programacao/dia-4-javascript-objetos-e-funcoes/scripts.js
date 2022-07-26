38dabc15438f1bbc0203dd5f624907151fcfa5e7
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2022-07-25 21:53:38 -0300

    lista de exercicíos 4.4 atualizada parte 01

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/scripts.js
@@ -1,27 +1,71 @@
-let info = {
-personagem: 'Margarida',
-origem: 'Pato Donald',
-nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
-recorrente: 'Sim'
-}
+// items 1,2,3,4 e 5
 
-for(key in info){
-console.log(key);
-}
+// let info = {
+// personagem: 'Margarida',
+// origem: 'Pato Donald',
+// nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+// recorrente: 'Sim'
+// }
 
-console.log('Bem-vinda',info.personagem);
+// for(key in info){
+// console.log(key);
+// }
 
-for(key in info) {
-    console.log(info[key]);
-}
+// console.log('Bem-vinda',info.personagem);
 
-let info2 = {
-    personagem: 'Tio Patinhas',
-    origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
-    nota: 'O último MacPatinhas Ambos recorrentes',
-    recorrente: 'Ambos recorrentes'
-}
-for(let key in info) {
-    for(let key in info2) {
-    console.log(info[key] , 'e', info2[key]);}
-}
+// for(key in info) {
+//     console.log(info[key]);
+// }
+
+// let info2 = {
+//     personagem: 'Tio Patinhas',
+//     origem: 'Christmas on Bear Mountain, Dells Four Color Comics #178',
+//     nota: 'O último MacPatinhas Ambos recorrentes',
+//     recorrente: 'Ambos recorrentes'
+// }
+// for(let key in info) {
+//     for(let key in info2) {
+//     console.log(info[key] , 'e', info2[key]);}
+// }
+
+// item 6,7 e 8.
+
+// let leitor = {
+//     nome: 'Julia',
+//     sobrenome: 'Pessoa',
+//     idade: 21,
+//     livrosFavoritos: [
+//       {
+//         titulo: 'O Pior Dia de Todos',
+//         autor: 'Daniela Kopsch',
+//         editora: 'Tordesilhas',
+//       },
+     
+//     ],
+//   };
+//   for(let key in leitor) {
+// console.log('O livro de', leitor.nome, leitor.sobrenome,'se chama', leitor.livrosFavoritos[0].titulo);
+//   }
+
+//   leitor.livrosFavoritos.push(
+//     {
+//         titulo: 'Harry Potter e o Prisioneiro de Azkaban',
+//         autor: 'JK Rowling',
+//         editora: 'Rocco',
+//     },
+//   );
+
+//   console.log(leitor['nome'], 'tem', leitor.livrosFavoritos.lenght, 'livros favoritos');
+
+// parte II - item 1
+
+// function checkPalindrome(string) {
+//     let reverse = string.split('').reverse().join('');
+//     if(string === reverse) {
+//         return true;
+//     }
+//         return false;
+// }
+
+// console.log(checkPalindrome('queijo'));
+// console.log(checkPalindrome('ama'));    

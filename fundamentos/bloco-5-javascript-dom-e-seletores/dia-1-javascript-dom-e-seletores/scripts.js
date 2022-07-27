8ca084a7900a014748aa7311ab4d5b293292fb84
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2022-07-27 18:04:02 -0300

    exercícios 5.1

---
+++ fundamentos/bloco-5-javascript-dom-e-seletores/dia-1-javascript-dom-e-seletores/scripts.js
@@ -0,0 +1,14 @@
+function changeText(){
+    let paragraph = document.getElementsByTagName('p')[1];
+        paragraph.innerHTML = "Estarei trabalhando na empresa dos meus sonhos";
+   
+}
+    changeText();
+
+function changeSquaretoGreen(){
+    let yellowSquare = document.getElementsByClassName('main-content')[0];
+        yellowSquare.style.background = "rgb(76,164,109)";
+}
+    changeSquaretoGreen();
+
+function 

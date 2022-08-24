77c4924662cc6806f2b5265527614a17e9b25b0f
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2022-08-23 23:25:03 -0300

    exercícios 8.3

--- fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-3-javascript-es6-higher-order-functions-map-e-filter/exercise1.js
@@ -61,7 +61,78 @@ const books = [
   },
 ];
 
-const formatedBookNames = books.map((book) => `${book.name.split(' ').join('_').toUpperCase()} - ${book.genre.split(' ').join('_').toUpperCase()} - ${book.author.split(' ').join('_').toUpperCase()}`
+const expectedResult = [
+  {
+    age: 31,
+    author: 'Isaac Asimov',
+  },
+  {
+    age: 38,
+    author: 'H. P. Lovecraft',
+  },
+  {
+    age: 39,
+    author: 'Stephen King',
+  },
+  {
+    age: 43,
+    author: 'George R. R. Martin',
+  },
+  {
+    age: 45,
+    author: 'Frank Herbert',
+  },
+  {
+    age: 62,
+    author: 'J. R. R. Tolkien',
+  },
+];
+const formatedBookNames = books.map((book) => `${book.name.split(' ').join('_').toUpperCase()} - ${book.genre.split(' ').join('_').toUpperCase()} - ${book.author.name.split(' ').join('_').toUpperCase()}`
 );
 
 console.log(formatedBookNames);
+
+function nameAndAge() {
+  return books.map(book => ({ 
+     author: book.author.name,
+     age: book.releaseYear - book.author.birthYear
+   }
+   )).sort((ageA, ageB) => ageA.age - ageB.age);
+   }
+   
+ console.log(nameAndAge());
+ 
+ function fantasyOrScienceFiction () {
+   return books.filter((book => book.genre === 'Fantasia' || book.genre === 'Ficção Científica'));
+ }
+ 
+ console.log(fantasyOrScienceFiction());
+ 
+ function oldBooksOrdered (){
+   const moreThan60Years =  books.filter((book) => 2022 - book.releaseYear >= 60);
+   const orderedBooks = moreThan60Years.sort((a,b) => (a.releaseYear - b.releaseYear));
+   return orderedBooks;
+ }
+ 
+ console.log(oldBooksOrdered());
+ 
+ function fantasyOrScienceFictionAuthors () {
+   const fantasyOrFiction = ['Fantasia','Ficção Científica'];
+   return books.filter((book) => fantasyOrFiction.includes(book.genre)).map((book) => book.author.name).sort();
+   }
+ 
+ console.log(fantasyOrScienceFictionAuthors());
+ 
+ function oldBooks () {
+   const currentYear = new Date().getFullYear();
+   return books.filter((book) => currentYear - book.releaseYear > 60).map((book) => book.name);
+ }
+ 
+ console.log(oldBooks());
+ 
+ function authorWith3DotsOnName () {
+ return books.find((book) => (book.author.name.split(' ').filter((word) => word.endsWith('.')).length === 3)).name;
+ }
+ 
+ console.log(authorWith3DotsOnName());
+ 

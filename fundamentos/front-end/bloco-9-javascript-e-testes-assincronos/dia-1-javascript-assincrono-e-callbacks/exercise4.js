18d431d4402e4950e927d7bc82de29203f878d65
Fabio Yuzo Matoba <fabioymatoba@uol.com.br>
2022-09-06 23:01:10 -0300

    exercícios 9.1

---
+++ fundamentos/front-end/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/exercise4.js
@@ -0,0 +1,12 @@
+const messageDelay = () => Math.floor(Math.random() * 5000);
+
+const getMarsTemperature = () => {
+  const maxTemperature = 58;
+  return Math.floor(Math.random() * maxTemperature);
+};
+
+const sendMarsTemperature = () => {
+  const currentTemperature = getMarsTemperature();
+  setTimeout(() => console.log(`A temperatura de Marte é: ${currentTemperature} graus Celsius`), messageDelay());
+}; 
+sendMarsTemperature(); 
